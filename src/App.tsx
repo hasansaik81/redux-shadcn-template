4ec6f5bbe43b521ac91f52bb70c1f906df95d746
hasansaik81 <hasansaik81@gmail.com>
2024-11-07 14:29:08 -0800

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button"
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h3>my app running </h3>
+      <Button>my button</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+// src/index.js
+
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import App from "./App";
+import React from "react";
+import  ReactDOM  from 'react-dom/client';
+
+
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+
+    <Provider store={store}>
+     <App/>
+    </Provider>
+
+</React.StrictMode>
+)
+
